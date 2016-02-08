c5f54380f92f615b708f8b9cb3fc4c713e669698
Cameron <cunninghamc@austin.rr.com>
2016-02-07 18:26:18 -0600

    Trying to get Routes to work.

--- Server.js
+++ Server.js
@@ -10,7 +10,24 @@ var http = require('http');
 
 app.use(express.static(__dirname + '/public'));
 
+app.get('searchbgg', function (req, res) {
+    
+    console.log("Server - Searchbgg");
+    var url = "https://www.boardgamegeek.com/xmlapi/search?search=Crossbows%20and%20Catapults"
 
+    console.log(url);
+    //xmlToJson(url, function(err, data) {
+    //    if (err) {
+    //        // Handle this however you like
+    //        return console.err(err);
+    //    }
+
+    //    // Do whatever you want with the data here
+    //    // Following just pretty-prints the object
+    //    console.log(JSON.stringify(data, null, 2));
+
+    //}
+    });   
 
 
 app.listen(3000);
--- public/features/services/controller.js
+++ public/features/services/controller.js
@@ -1,15 +1,12 @@
 ﻿var app = angular.module('MAapp', []);
-app.controller('MACtrl', function ($scope, $http) {
-    var url = "https://www.boardgamegeek.com/xmlapi/search?search=Crossbows%20and%20Catapults"
+app.controller('MACtrl',['$scope', '$http', function ($scope, $http) {
+    
+    console.log("Controller - Start");
 
-    xmlToJson(url, function(err, data, $http) {
-        if (err) {
-            // Handle this however you like
-            return console.err(err);
-        }
+    $http.get('/searchbgg').success(function (response) {
+        console.log("Controller - search results");
 
-        // Do whatever you want with the data here
-        // Following just pretty-prints the object
-        console.log(JSON.stringify(data, null, 2));
-    });
-});
+        $scope.searchresults = response;
+    })
+
+}]);
